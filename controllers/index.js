22cd261cdc647b49598a42d20c0f56bf3451187a
scottsc0de <scott5902@gmail.com>
2022-12-30 14:00:16 -0500

    setup controller index

--- controllers/index.js
+++ controllers/index.js
@@ -4,8 +4,12 @@ const apiRoutes = require('./api');
 const homeRoutes = require('./homepage-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
 
-router.use('/', apiRoutes);
+router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
-router.use('/', dashboardRoutes);
+router.use('/dashboard', dashboardRoutes);
+
+router.use((req, res) => {
+    res.status(404).end();
+});
 
 module.exports = router;
